3685984e13b8bbacadb780c311b76384cc904543
ZackCCon <128091404+ZackCCon@users.noreply.github.com>
2023-03-16 15:14:04 -0500

    Update streamlit.py
    Add dataframe

--- streamlit.py
+++ streamlit.py
@@ -11,3 +11,4 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
