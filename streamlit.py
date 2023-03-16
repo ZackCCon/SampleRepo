c9d96837582656723e0c898af23dd82d89e02cb9
ZackCCon <128091404+ZackCCon@users.noreply.github.com>
2023-03-16 15:13:32 -0500

    Update streamlit.py
    Added pandas test

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Zack has created an app')
 
@@ -8,3 +9,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
