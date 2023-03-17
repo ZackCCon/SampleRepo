8060842d7b76fbee37e4d747323eb89ffbdfd3ca
ZackCCon <128091404+ZackCCon@users.noreply.github.com>
2023-03-17 13:35:54 -0500

    Update streamlit.py
    Add fruit text input

--- streamlit.py
+++ streamlit.py
@@ -17,6 +17,9 @@ streamlit.dataframe(fruits_to_show)
 #API Header
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #API Response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
 
