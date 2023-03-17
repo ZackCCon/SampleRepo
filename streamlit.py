354879bd463127e8a93bd3ecfced5780a4923142
ZackCCon <128091404+ZackCCon@users.noreply.github.com>
2023-03-17 13:32:29 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,6 +19,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #API Response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
