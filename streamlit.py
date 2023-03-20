abc287caa223084621fcc80e6fdaf4a5c3c4e949
ZackCCon <128091404+ZackCCon@users.noreply.github.com>
2023-03-20 12:04:12 -0500

    Update streamlit.py
    Updated with Try functionality

--- streamlit.py
+++ streamlit.py
@@ -18,19 +18,20 @@ streamlit.dataframe(fruits_to_show)
 
 #API Header
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#API Response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
-
-# Normalize API Response data 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display Normalized data as Dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not frujit_choice:
+    streamlit.error("Please select a fruit")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
+
 # Snowflake Steps Begin Below
 
 # Get Snowflake metadata
